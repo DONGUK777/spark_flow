c0bba3912f850cf68295bc9b75629b16f84aae2d
DONGUK777 <kimdongyork@gmail.com>
2024-08-08 12:22:10 +0900

    some edit

--- dags/pyspark_movie.py
+++ dags/pyspark_movie.py
@@ -36,26 +36,6 @@ with DAG(
     tags=['spark', 'movie', 'ant'],
 ) as dag:
   # t1, t2 and t3 are examples of tasks created by instantiating operators
-##    def get_data(ds_nodash):
-#        from mov.api.call import save2df
-#        df = save2df(ds_nodash)
-#        print(df.head(5))
-
-    def fun_multi_y(ds_nodash, url_param):
-        from mov.api.call import save2df
-        df = save2df(load_dt=ds_nodash, url_param=url_param)
-        
-        print(df[['movieCd', 'movieNm']].head(5))
-
-        for k, v in url_param.items():
-            df[k] = v
-
-        #p_cols = list(url_param.keys()).insert(0, 'load_dt')
-        p_cols = ['load_dt'] + list(url_param.keys())
-        df.to_parquet('~/tmp/test_parquet',
-                partition_cols=p_cols
-                # partition_cols=['load_dt', 'movieKey']
-        )
 
 
     def repartition(ds_nodash):
